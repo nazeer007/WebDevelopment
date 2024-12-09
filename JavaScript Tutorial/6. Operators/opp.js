7933140b19bfce63f24e7ee0865596e2a8bb44c2
Syed Nazeer <109338083+nazeer007@users.noreply.github.com>
2024-12-09 11:22:24 +0530

    first commit

---
+++ JavaScript Tutorial/6. Operators/opp.js
@@ -0,0 +1,13 @@
+//Addition
+let x = 10;
+let y = 20;
+let sum = x + y;
+document.getElementById('demo1').innerHTML="Addition: "+sum;
+//Multiplication
+let mul = x * y;
+document.getElementById('demo2').innerHTML="Multiplication: "+mul;
+
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+document.getElementById("demo3").innerHTML = "Is A less than B? " + result;
